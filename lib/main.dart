5444576456fe25bc33d711918a522e00523d9c5a
Sydney <sydney@recroom.com>
2022-04-18 17:40:13 -0700

    inital set of ui elements

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cereal/menus/bottomnav.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -75,6 +76,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
+      bottomNavigationBar: bottomNavBar(context),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
@@ -96,7 +98,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'You have pressed the button this many times:',
             ),
             Text(
               '$_counter',
---
+++ lib/menus/bottomnav.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:functional_widget_annotation/functional_widget_annotation.dart';
+
+@swidget
+Widget bottomNavBar(BuildContext ctx) {
+    return _bottomNavBarTheme(ctx, 
+        child: Row(children: 
+            <Widget>[
+                _bottomNavBarIcon(ctx, icon: Icons.library_books, text: 'Library'),
+                _bottomNavBarIcon(ctx, icon: Icons.new_releases, text: 'Updates'),
+                _bottomNavBarIcon(ctx, icon: Icons.history, text: 'History'),
+                _bottomNavBarIcon(ctx, icon: Icons.browser_updated, text: 'Browse'),
+                _bottomNavBarIcon(ctx, icon: Icons.settings, text: 'More'),
+
+            ],
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        )
+    );
+}
+
+Widget _bottomNavBarTheme(BuildContext ctx, {required Widget child}){
+    return BottomAppBar(
+        child: IconTheme(
+            data: IconThemeData(),
+            child: child
+        ),
+        color: Colors.blue
+    );
+}
+
+Widget _bottomNavBarIcon(BuildContext ctx, {required IconData icon, required String text}){
+    return Container(
+        margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+        child: Column(
+            children: <Widget>[
+                Icon(icon),
+                Text(text)
+            ],
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        )
+    );
+}
